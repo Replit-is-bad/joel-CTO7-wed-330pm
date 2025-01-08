8429b57969be44297c50aa63ae41c2a876e1906b
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-08 09:03:45 +0000

    Jan 8, 2025, 9:03 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,32 @@
 print("Hello from lesson 1")
 
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+# average_score = total / 3
+# student_name = "Alex"
+# print("average score for" + "student_name" + " is: " + str(average_score))
+
+
+
+# grade = int(input("what is your score? "))
+# if grade >75 :
+#     print("your grade is A")
+# if grade >60 :
+#     print("your grade is B")
+# if grade >59 :
+#     print("your grade is C")
+# else :
+#     print("you failed")
+
+
+
+# for i in range(0,21):
+#     print(i)
+# for i in range(1,31):
+#     print(i)
+# for i in range(2,25,2):
+#     print(i)
+

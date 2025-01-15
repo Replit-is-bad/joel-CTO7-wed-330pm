e75752d093ed08d0b2703801adcdb5faf6617acc
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-15 08:31:15 +0000

    Jan 15, 2025, 8:31 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -56,4 +56,4 @@ print('welcome to hizza put')
 while True :
     top = input("What toppings do you want?type 'end' to stop: ")
     if top == 'end':
-        print("thank you")
+        print("Thank you.")

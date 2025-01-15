9f539d7d9f8ad0af3bbff11247abf53d615679a8
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-15 08:34:25 +0000

    Jan 15, 2025, 8:34 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -52,7 +52,7 @@ import random
         
 topping=''
 
-print('welcome to  azzip ')
+print('welcome to tuh azzip ')
 while True :
     top = input("What toppings do you want?type 'end' to stop: ")
     if top == 'end':

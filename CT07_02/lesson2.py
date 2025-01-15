dbcae96b6ffdce3faa99d4e760e6bf383ab5d687
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-15 07:49:50 +0000

    Jan 15, 2025, 7:49 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,17 +1,18 @@
 print("Hello from lesson 2")
 
 
-count = 2
-while count <= 24:
-    print(count)
-    count = count + 1
-count = 1
-while count <= 30:
-    print(count)
-    count = count + 1
-count = 0
-while count <= 20:
-    print(count)
-    count = count + 1
-    #count += 1
+# count = 2
+# while count <= 24:
+#     print(count)
+#     count = count + 1
+# count = 1
+# while count <= 30:
+#     print(count)
+#     count = count + 1
+
+# count = 0
+# while count <= 20:
+#     print(count)
+#     count = count + 1
+#     #count += 1
 

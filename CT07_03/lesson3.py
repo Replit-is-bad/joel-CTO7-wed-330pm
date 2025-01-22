1ffdc93024153a2fdef10a43bf843571bc20cfd6
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-22 07:46:57 +0000

    Jan 22, 2025, 7:46 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -4,6 +4,6 @@ while True:
 
     ans=input(' I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I? ')
     if ans != 'idk':
-        print
+        print('you')
 
 

ec3b63b45f2445e4315e0d254012ac6cbddcd0e1
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-22 08:12:26 +0000

    Jan 22, 2025, 8:12 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -12,7 +12,17 @@ import time
 
 duration = int(input('how long(in min) do you want to study for? '))
 mode=input('would you like to count down or up?')
+if mode =='up':
+    for i in range (duration,0,-1):
+        print(f'you have {i} seconds remaining')
+    time.sleep(1)
+elif mode =='down':
+    counter = 1
+    while True:
+        print(str(f'{counter} seconds past'))
+        time.sleep(1)
+        counter += 1
+else:
+    print('try again')
+    
 
-for i in range (duration,0,-1):
-    print(f'you have {i} seconds remaining')
-    time.sleep(60)

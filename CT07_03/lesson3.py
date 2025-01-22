a19ec9aa76dc6ac735640f882dc39848fea7ced7
Replit-is-bad <ylchia_hill@hotmail.com>
2025-01-22 07:44:06 +0000

    Jan 22, 2025, 7:44 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -2,5 +2,5 @@ print("Hello from lesson 3")
 ans='echo'
 while True:
 
-    print(' I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I? ')
+    input(' I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I? ')
 

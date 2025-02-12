96205911f793b54a3c2e23539b4fece829cec111
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:09:15 +0000

    Feb 12, 2025, 8:09 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -17,7 +17,7 @@ numm =[
 while len(numm) <= 100:
     num2=(random.randint(1,1000))
     while num2 in numm:
-        num2=(int(random.randint(1,1000)))
+        num2=(random.randint(1,1000))
     numm.append(num2)
 
 for i in numm:

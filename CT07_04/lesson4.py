b93ba53a9c4103d131179dd9196fccc9e8fb4cad
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 07:31:43 +0000

    Feb 12, 2025, 7:31 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,5 +1,83 @@
+import time
+# counter=10
+# while counter >= 0:
+#     print(counter)
+#     time.sleep(1)
+#     counter -= 1
+# print('happy new year')
 
-counter=10
-while counter >= 1:
-    print(counter)
-print('happy new year')
+# planets = [
+#     "Mercury",
+#     "Venus",
+#     "Earth",
+#     "Mars",
+#     "Jupiter",
+#     "Saturn",
+#     "Uranus",
+#     "Neptune"
+# ]
+
+# # planets.append ('Pluto')
+# planets.insert(3, "lalaland")
+
+# remove=planets.pop(5)
+
+# # planets [3] = 'id win'
+
+# for i in planets:
+#     if i =="Earth":
+#         print(i + ": my home")
+#     elif i == "Mars":
+#         print(i + ": i conquered this")
+#     elif i =="lalaland":
+#         print(i + ": i made this")
+#     else:
+#         print(i)
+
+
+
+
+#list.insert to insert a item to the list
+#list.append to add into the end
+
+#list.pop(num)
+
+#del(list[num])
+
+#print(len(list))is to see how much items are there in your list
+
+#for i in range(0,len(list)-1):
+#   print(list[i] + "," + list [i+1])
+
+# country = input("what countrys do u want to visit")
+# contries =[]
+# while country != "end":
+#     contries.append(country)
+#     country = input("what countrys do u want to visit ")
+# for i in contries:
+#     print("I would like to visit " + i)
+
+food = input("what u want?? ")
+cost =input("how much does it cost?? ")
+morefood =[]
+moeny=[]
+while food != "end":
+    morefood.append(food)
+    moeny.append(cost)
+    food = input("what u want?? ")
+    cost =input("how much does it cost?? ")
+
+
+custo=input("what would you like to order?? ")
+order =False
+for i in morefood:
+    if i == custo:
+        print("ok we have " + i)
+        order = True
+for i in range(len(moeny)):
+   print(morefood[i] + ": $" + moeny[i])
+if order==False:
+    print("DIEE")
+
+    
+

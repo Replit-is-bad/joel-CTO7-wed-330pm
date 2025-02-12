e62ab952e1cd20ea1e9919bd8afdacbffabb70e9
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:25:55 +0000

    Feb 12, 2025, 8:25 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -41,5 +41,16 @@ import random
 # print(str(hig) + guyh)
 
 
-
-
+pokemons = [
+    "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+    "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+    "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+    "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+    "Electabuzz"
+]
+powers = [
+    55, 84, 49, 48, 45,
+    45, 52, 55, 110, 110,
+    85, 65, 134, 130, 110,
+    50, 125, 65, 110, 83
+]

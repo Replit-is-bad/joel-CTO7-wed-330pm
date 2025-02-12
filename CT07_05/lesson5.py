654386544acd44c58d6d35d2f8bdff64bde4209f
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:08:15 +0000

    Feb 12, 2025, 8:08 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -25,7 +25,7 @@ for i in numm:
 
 maxi=max(numm)
 minn=min(numm)
-print(str('the max is : '+maxi))
-print(str('the min is : '+minn))
+print(str('the max is : '+ maxi))
+print(str('the min is : '+ minn))
 
 

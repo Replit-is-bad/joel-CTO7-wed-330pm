1a4704e78b848f21fffba670d69dc577dcfaea96
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 07:40:54 +0000

    Feb 12, 2025, 7:40 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,13 +1,14 @@
-food =[
-    'ham',
-    'burg',
-    'er',
-    'bread',
-    'candy'
-]
-food.pop(2)
-food.append('more candy')
-for i in food:
-    print(i)
+
+# food =[
+#     'ham',
+#     'burg',
+#     'er',
+#     'bread',
+#     'candy'
+# ]
+# food.pop(2)
+# food.append('more candy')
+# for i in food:
+#     print(i)
 
 

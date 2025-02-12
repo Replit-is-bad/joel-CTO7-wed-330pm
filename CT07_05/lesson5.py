d36d0147b5bc7c10ac8d14f428b50909487f196f
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:15:59 +0000

    Feb 12, 2025, 8:15 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -29,3 +29,10 @@ import random
 # print('the min is : '+ str(minn))
 
 
+
+namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
+            "Sophia", "Lucas", "Mia", "Aiden"
+            ]
+heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+
+

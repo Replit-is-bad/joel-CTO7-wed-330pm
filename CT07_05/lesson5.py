0ebc37056d9414bfdb8046b08f21f0ee4719bbd0
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:10:46 +0000

    Feb 12, 2025, 8:10 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -15,7 +15,7 @@ numm =[
 
 ]
 while len(numm) <= 100:
-    num2=str(random.randint(1,1000))
+    num2=(random.randint(1,1000))
     while num2 in numm:
         num2=(random.randint(1,1000))
     numm.append(num2)
@@ -26,6 +26,6 @@ for i in numm:
 maxi=max(numm)
 minn=min(numm)
 print('the max is : '+ maxi)
-print('the min is : '+ minn)
+print( minn)
 
 

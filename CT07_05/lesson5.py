c8c4f233bf35ca516b8031a7364e702d47d14ab2
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:14:15 +0000

    Feb 12, 2025, 8:14 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -11,21 +11,21 @@ import random
 # for i in food:
 #     print(i)
 
-numm =[
+# numm =[
 
-]
-while len(numm) <= 100:
-    num2=(random.randint(1,1000))
-    while num2 in numm:
-        num2=(random.randint(1,1000))
-    numm.append(num2)
+# ]
+# while len(numm) <= 100:
+#     num2=(random.randint(1,1000))
+#     while num2 in numm:
+#         num2=(random.randint(1,1000))
+#     numm.append(num2)
 
-for i in numm:
-    print(i)
+# for i in numm:
+#     print(i)
 
-maxi=max(numm)
-minn=min(numm)
-print('the max is : '+ str(maxi))
-print('the min is : '+ str(minn))
+# maxi=max(numm)
+# minn=min(numm)
+# print('the max is : '+ str(maxi))
+# print('the min is : '+ str(minn))
 
 

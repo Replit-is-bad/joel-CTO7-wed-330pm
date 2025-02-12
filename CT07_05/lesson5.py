27649342c506f4c1f3a0e057f89ac30722f43ad9
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-12 08:16:07 +0000

    Feb 12, 2025, 8:16 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -31,8 +31,8 @@ import random
 
 
 namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
-            "Sophia", "Lucas", "Mia", "Aiden"
-            ]
+  "Sophia", "Lucas", "Mia", "Aiden"
+ ]
 heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 
 

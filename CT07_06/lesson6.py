d639e96776c9a6f6b73db138f8312de1635d0934
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-19 08:32:22 +0000

    Feb 19, 2025, 8:32 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -2,11 +2,11 @@ import random
 import time
 list101=[]
 
-# for counter in range(100):
-#     num=random.randint(1,100)
-#     while num in list101:
-#         num=random.randint(1,100)
-#     list101.append(num)
+for counter in range(100):
+    num=random.randint(1,100)
+    while num in list101:
+        num=random.randint(1,100)
+    list101.append(num)
 
 # for i in list101:
 #     print(i)

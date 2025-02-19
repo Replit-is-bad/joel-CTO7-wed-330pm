76cb404a9ee8b183aff5c71110c5f133fc971e11
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-19 07:48:50 +0000

    Feb 19, 2025, 7:48 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -7,8 +7,8 @@ for counter in range(100):
     while num in list101:
         num=random.randint(1,100)
     list101.append(num)
-    
+
 for i in list101:
     print(i)
     
-
+print('the highest num is ')

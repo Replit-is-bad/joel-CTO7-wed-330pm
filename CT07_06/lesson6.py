3dbf497d6f9b34b48dbd6f46f9cd559a335962e9
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-19 08:05:45 +0000

    Feb 19, 2025, 8:05 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -22,7 +22,8 @@ contact1 = ["John", 98453126, "john@gmail.com"]
 contact2 = ["Adam", 93029102, "adam@gmail.com"]
 contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
 
-contacts.append(contacts)
+
 
 # for shelf in bookshelf:
 #     for item in shelf:
+contacts.append(contact1)

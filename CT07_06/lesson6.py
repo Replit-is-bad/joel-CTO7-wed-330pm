a46d0a94650ae186d28bce55c322c6094a6ca069
Replit-is-bad <ylchia_hill@hotmail.com>
2025-02-19 07:45:27 +0000

    Feb 19, 2025, 7:45 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -6,6 +6,7 @@ for counter in range(100):
     num=random.randint(1,100)
     while num in list101:
         num=random.randint(1,100)
-    list101.append()
+    list101.append(num)
+print()
     
 

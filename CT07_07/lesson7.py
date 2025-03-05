b73671ff2d4996b188b59ab8742196a43fa85d48
Replit-is-bad <ylchia_hill@hotmail.com>
2025-03-05 07:32:58 +0000

    Mar 5, 2025, 7:32 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -60,6 +60,11 @@
 #         even.append(dud)
 
 # print(even)
-
+new=[]
 students = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 size = 3
+
+for i in range(0,len(students),size):
+    new.append(students[i:i+size])
+
+print(new)

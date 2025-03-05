e3891d3ffbfe03978865781f8440e1c2c6f55378
Replit-is-bad <ylchia_hill@hotmail.com>
2025-03-05 08:09:45 +0000

    Mar 5, 2025, 8:09 AM

--- temp.py
+++ temp.py
@@ -12,4 +12,4 @@ print('Hero starts on his adventure with Health: ' + str(herohp))
 while herohp != 0:
     dmg = random.randint(1,15)
     herohp -= dmg
-    print('After fighting monsters, his Health is now: ' + herohp)
+    print('After fighting monsters, his Health is now: ' + str(herohp))

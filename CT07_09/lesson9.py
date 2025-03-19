09cb3c6ddc3f230f93c5472fd0163437670d6695
Replit-is-bad <ylchia_hill@hotmail.com>
2025-03-19 08:21:47 +0000

    Mar 19, 2025, 8:21 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -18,9 +18,9 @@ window.setup(width=700, height=500)
 window.bgcolor('gold3')
 
 t = turtle.Turtle()
-t.shape("circlesu")
+t.shape("square")
 t.fillcolor("forestgreen")
 t.speed(10)
 
-t.stamp
+t.stamp()
 window.mainloop()

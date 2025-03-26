d8b7f2fda83ff8562f05ac28d9abf31bce20c1ea
Replit-is-bad <ylchia_hill@hotmail.com>
2025-03-26 08:22:52 +0000

    Mar 26, 2025, 8:22 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,27 +1,27 @@
-import turtle
+# import turtle
 
 
 
-window = turtle.Screen()
-window.setup(width=900 ,height=800)
+# window = turtle.Screen()
+# window.setup(width=900 ,height=800)
 
-t = turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
-t.speed(10)
-t.seth(0)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
+# t.speed(10)
+# t.seth(0)
 
-def square():
-    count = 3
-    math = 360/count
+# def square():
+#     count = 3
+#     math = 360/count
 
-    while True: 
-        for i in range (count):
-            t.forward(100)
-            t.left(math)
-        count += 1
-        math = 360/count
-square()v
+#     while True: 
+#         for i in range (count):
+#             t.forward(100)
+#             t.left(math)
+#         count += 1
+#         math = 360/count
+# square()
 # def muliyp(a,b):
 #     print(a*b)
 # muliyp(3,5)

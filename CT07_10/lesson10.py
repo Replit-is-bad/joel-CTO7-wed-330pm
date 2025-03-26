ff4dffea78aae2e7f91786e2b2f2c43f575da8bf
Replit-is-bad <ylchia_hill@hotmail.com>
2025-03-26 08:00:37 +0000

    Mar 26, 2025, 8:00 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,23 +1,23 @@
-import turtle
+# import turtle
 
 
 
-window = turtle.Screen()
-window.setup(width=600 ,height=500)
+# window = turtle.Screen()
+# window.setup(width=600 ,height=500)
 
-t = turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
-t.speed(10)
-t.seth(0)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
+# t.speed(10)
+# t.seth(0)
 
-count = 3
-math = 360/count
+# count = 3
+# math = 360/count
 
-while True: 
-    for i in range (count):
-        t.forward(100)
-        t.left(math)
-    count += 1
-    math = 360/count
+# while True: 
+#     for i in range (count):
+#         t.forward(100)
+#         t.left(math)
+#     count += 1
+#     math = 360/count
 

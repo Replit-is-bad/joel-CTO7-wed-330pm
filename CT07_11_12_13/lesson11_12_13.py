a7ea8d9a40dff11f474f47e12fa22de2e6e395de
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-02 08:05:45 +0000

    Apr 2, 2025, 8:05 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -10,13 +10,13 @@
         
 # print(diceGuess(4))
 
+def initboard():
+    board = []
 
-board = []
-
-for i in range(3):
-    row = []
-    for j in range(3):
-        row.append(" ")
-    board.append(row)
+    for i in range(3):
+        row = []
+        for j in range(3):
+            row.append(" ")
+        board.append(row)
 
 print(board)

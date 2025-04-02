cdb339db293531dd3313f45cd4c3dcbed4a86a23
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-02 08:01:40 +0000

    Apr 2, 2025, 8:01 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -1,11 +1,13 @@
-import random
+# import random
 
 
-def diceGuess(par1):
-    rNum1 = random.randint(1,6)
-    if rNum1 == par1:
-        return True
-    else:
-        return False
+# def diceGuess(par1):
+#     rNum1 = random.randint(1,6)
+#     if rNum1 == par1:
+#         return True
+#     else:
+#         return False
         
-print(diceGuess(4))
+# print(diceGuess(4))
+
+

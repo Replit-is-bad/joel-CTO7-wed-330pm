3534d21017becde9a6c0f313028e72090b9121e7
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-09 08:40:51 +0000

    Apr 9, 2025, 8:40 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -90,7 +90,7 @@ def checkwin(argboard):
     for condition in win:
         if condition[0] == condition[1] == condition[2] and condition[0] !='':
             return True
-    
+    return f
 #Main game loop
 board = initboard()
 cplayer =''

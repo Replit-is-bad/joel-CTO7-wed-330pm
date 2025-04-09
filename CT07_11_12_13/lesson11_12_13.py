41c9cf85d6e049c8528422f70a874e6a332ce57a
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-09 08:36:42 +0000

    Apr 9, 2025, 8:36 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -82,6 +82,7 @@ def checkwin(argboard):
 [argboard[0][1],argboard[1][1], argboard[2][1]]#col 2
 [argboard[0][2],argboard[1][2], argboard[2][2]]#col 3
 
+[argboard[0][0],argboard[1][1], argboard[2][2]]
 [argboard[0][0],argboard[1][0], argboard[2][0]]
 ]
 

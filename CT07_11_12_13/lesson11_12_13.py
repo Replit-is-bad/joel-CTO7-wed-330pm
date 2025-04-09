32c1cff0df99c2523d80f383845eab00f4c21725
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-09 08:33:17 +0000

    Apr 9, 2025, 8:33 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -75,7 +75,8 @@ def getcurrentplayer(player):
 def checkwin(argboard):
     win = [
 [argboard[0][0],argboard[0][1], argboard[0][2]]
-[argboard[1][0],argboard[0][1], argboard[0][2]]
+[argboard[1][0],argboard[1][1], argboard[1][2]]
+[argboard[2][0],argboard[0][1], argboard[0][2]]
 ]
 
 

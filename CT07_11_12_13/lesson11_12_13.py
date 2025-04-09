87ffbeea5e086634867f0243496fd86bd91be478
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-09 07:48:16 +0000

    Apr 9, 2025, 7:48 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -52,7 +52,7 @@ while True:
             if board[row][col] == " ":
                 board[row][col] ='X' 
             else:
-                print(f'{pMove + 1 } is already ')
+                print(f'{pMove + 1 } is already eaten by :')
         else:
             print('ERROR :.... .- ...- . / .- / -. .. -.-. . / -.. .- -.-- .-.-.- .-.-.- .-.-.- . .-.. ... . .-- .... . .-. . .-.-.-')
     else:

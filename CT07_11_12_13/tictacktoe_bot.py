48e8e4bbf29ab7279e7ca1c7f002ddd4ee126bdf
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-16 07:49:34 +0000

    Apr 16, 2025, 7:49 AM

--- CT07_11_12_13/tictacktoe_bot.py
+++ CT07_11_12_13/tictacktoe_bot.py
@@ -109,7 +109,8 @@ def get_bot_move(board, currplayer):
     for i in range(3):
         for j in range(3):
             if board[i][j]== " ":
-                
+                board
+
 
 #Main game loop
 board = initboard()

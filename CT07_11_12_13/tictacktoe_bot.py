3ec565c200309e4951604e3907c1ba8a4396f780
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-16 08:43:38 +0000

    Apr 16, 2025, 8:43 AM

--- CT07_11_12_13/tictacktoe_bot.py
+++ CT07_11_12_13/tictacktoe_bot.py
@@ -106,6 +106,7 @@ def checktie(argboard):
 # 4. If a corner is empty == take a corner.
 # 5. Else == take any available space.
 def get_bot_move(board, currplayer):
+    
     for i in range(3):
         for j in range(3):
             if board[i][j]== " ":

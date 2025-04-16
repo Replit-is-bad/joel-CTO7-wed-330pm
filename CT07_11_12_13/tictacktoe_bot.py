0e38ef6990c783933f525b07cc51878f69b30924
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-16 08:42:26 +0000

    Apr 16, 2025, 8:42 AM

--- CT07_11_12_13/tictacktoe_bot.py
+++ CT07_11_12_13/tictacktoe_bot.py
@@ -137,7 +137,7 @@ def get_bot_move(board, currplayer):
 
     for i , j in conners:
         if board [i,j]== " ":
-            bo
+            board[i][j] =currplayer
 
 #Main game loop
 board = initboard()

7cafff66a96da8ef5895e474ffa89ecf6a282bbf
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-30 08:47:29 +0000

    Apr 30, 2025, 8:47 AM

--- Wordle.py
+++ Wordle.py
@@ -59,6 +59,7 @@ for chance in range(1,7):
         else:
             display_check = display_check + '#'
 
+    print
     print(f'You guess : {checkguess}')
     print(f'Result    : {display_check}')
 

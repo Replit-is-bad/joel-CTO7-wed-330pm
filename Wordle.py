379fc5e8722e33fbc8dbae252db0bb9c257a5dff
Replit-is-bad <ylchia_hill@hotmail.com>
2025-04-30 08:55:05 +0000

    Apr 30, 2025, 8:55 AM

--- Wordle.py
+++ Wordle.py
@@ -66,7 +66,7 @@ for chance in range(1,7):
 
     if checkguess == wordle:
         print('You are right!')
-        print(f'{checkguess}')
+        print(f'{checkguess} is the answer!')
 else:
     print('')
     print(f'THe actual word was {wordle}')

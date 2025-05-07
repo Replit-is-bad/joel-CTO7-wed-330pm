4e9e67d88add67e19f6ee2e84d4b6b6dd76f750a
Replit-is-bad <ylchia_hill@hotmail.com>
2025-05-07 08:18:01 +0000

    May 7, 2025, 8:18 AM

--- endsem/ct07sem2q2.py
+++ endsem/ct07sem2q2.py
@@ -35,7 +35,6 @@ list1 = [2944, 5490, 2357, 2619, 1177, 451, 8299, 2533, 4682, 6040,
 
 ###### Task 2: COMPLETE THE FOR LOOP TO CALL is_even() here.
 for i in list1:
-    is_even(i)
     if is_even(i) == True:
         print(f'{i} is even.')
     else:

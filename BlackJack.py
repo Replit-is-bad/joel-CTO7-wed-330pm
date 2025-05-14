ff011632ebebcea8069c58cf6f71d8386246d0b4
Replit-is-bad <ylchia_hill@hotmail.com>
2025-05-14 08:06:23 +0000

    May 14, 2025, 8:06 AM

--- BlackJack.py
+++ BlackJack.py
@@ -44,7 +44,7 @@ def show_hand(hand,playtype):
         print('@' * 15)
 
     elif playtype == 'banker_hide':
-        pei
+        print('$')
         print(f'{hand[0][1]} {hand[0][0]}')
 
 show_hand(player_hand, 'player_show')

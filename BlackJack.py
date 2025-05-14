25d555ed72fed416fd2588436993fb9a543e75d7
Replit-is-bad <ylchia_hill@hotmail.com>
2025-05-14 07:43:55 +0000

    May 14, 2025, 7:43 AM

--- BlackJack.py
+++ BlackJack.py
@@ -3,7 +3,12 @@ import random
 suits = ["♣ CLUB", "♦ DIAMOND","❤ HEART","♠ SPADE"]
 ranks = ['2','3','4','5','6','7','8','9','JACK','QUEEN','KING','ACE']
 
-
+# Step 1: Create the deck of cards
+# Create a dictionary to store the value of each card rank
+# Cards 2–10 are worth their face value, J, Q, K are worth 10, 
+# and Ace can be 1 or 11
+values = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+'JACK':10, 'QUEEN':10,'KING':10, 'ACE':11}
 
 # deck of cards
 deck = []

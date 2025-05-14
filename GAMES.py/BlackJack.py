c230679bbf860162070c9678330f1eaa3845ce6c
Replit-is-bad <ylchia_hill@hotmail.com>
2025-05-14 08:41:40 +0000

    May 14, 2025, 8:41 AM

--- GAMES.py/BlackJack.py
+++ GAMES.py/BlackJack.py
@@ -100,4 +100,5 @@ while True:
             while calculate(banker_hand) <= 17:
                 print('$$$ BANKER IS THINKING $$$$')
                 time.sleep(1)
-                banker_hand.append(deck.pop)
+                banker_hand.append(deck.pop())
+                

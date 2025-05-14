68c37924d84e0f9e0f24cec9edcd892f482c76ba
Replit-is-bad <ylchia_hill@hotmail.com>
2025-05-14 08:31:46 +0000

    May 14, 2025, 8:31 AM

--- BlackJack.py
+++ BlackJack.py
@@ -84,6 +84,6 @@ while True:
         print('YOU WIN ! BLACK JACK !')
         break
     else:
-        action = input(" 1 - hit , 2 - check")
+        action = input(" 1 - hit , 2 - check : ")
         if action == "1":
             player_hand.append(deck.pop())

04020322e27d2d8f383986e279f93d1c9c3ba4d8
Replit-is-bad <ylchia_hill@hotmail.com>
2025-05-14 07:58:53 +0000

    May 14, 2025, 7:58 AM

--- BlackJack.py
+++ BlackJack.py
@@ -38,7 +38,7 @@ def show_hand(hand,playtype):
     if playtype == "player_show":
         # show the player's hand
         for card in hand:
-            print(f'{card[0]}')
+            print(f'{card[1]} {card[0]}')
 
 show_hand(player_hand, 'player_show')
 
